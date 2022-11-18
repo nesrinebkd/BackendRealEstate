6c218c843726772234732a621ad8cb749cbe824f
nesrinebkd <nesrineblkd@gmail.com>
2022-11-18 17:32:18 +0100

    modified:   .env 	modified:   package-lock.json 	modified:   package.json 	modified:   routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 // REGISTER
 router.post('/register', async (req, res) => {
   const newUser = new User({
@@ -32,8 +33,17 @@ router.post('/login', async (req, res) => {
 
     Originalpassword !== req.body.password &&
       res.status(401).json('Wrong password');
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '3d' }
+    );
     const { password, ...others } = user._doc;
-    Originalpassword == req.body.password && res.status(200).json(user);
+    Originalpassword == req.body.password &&
+      res.status(200).json({ others, accessToken });
   } catch (error) {
     res.status(500).json(err);
   }
