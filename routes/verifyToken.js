4c971a0292bfd037302e04f37b11c69e77d5d54e
nesrinebkd <nesrineblkd@gmail.com>
2022-11-18 20:46:56 +0100

    new file:   routes/verifyToken.js

---
+++ routes/verifyToken.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    jwt.verify(token, proces.env.JWT_SECRET, (err, user) => {
+      if (err) res.status(403).json('token is not valid');
+      res.user = user;
+      next(); // go finish ur route
+    });
+  } else {
+    return res.status(401).json('You are not authenticated!');
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json('You are not allowed to do that !');
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status.json('You are not allowed to do that!');
+    }
+  });
+};
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
